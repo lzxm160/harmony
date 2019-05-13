03056cd355629f097efa652c973fb938a35203e2
lzxm160 <lzxm160@sina.com>
2019-05-13 18:54:05 +0800

    x

--- api/proto/discovery/pingpong_test.go
+++ api/proto/discovery/pingpong_test.go
@@ -1,7 +1,6 @@
 package discovery
 
 import (
-	"fmt"
 	"reflect"
 	"strings"
 	"testing"
@@ -20,8 +19,8 @@ var (
 		Port:            "9999",
 		ConsensusPubKey: pubKey1,
 	}
-	e1 = `{1 IP:"127.0.0.1" Port:"9999" PubKey:"x\001\202\305\036\312N\354T\371\005\346\204\320\362\362\366?d{`\013\323\344\0048@^9\205\003\342\376\336\347\240\262Q\374\315(\034-\002ZJ\317\017DV\212D\217\260\335\241li\205\006@y\\\031\206\377\t\321\234\321w\273\r\240\027\223\360\030\304\230d\024\2433v-d\032\263\343\270\246\223q2\213" PeerID:"<peer.ID >"  {} [] 0}`
-	e3 = "ping:Client/1=>127.0.0.1:9999/[120 1 130 197 30 202 78 236 84 249 5 230 132 208 242 242 246 63 100 123 96 11 211 228 4 56 64 94 57 133 3 226 254 222 231 160 178 81 252 205 40 28 45 2 90 74 207 15 68 86 138 68 143 176 221 161 108 105 133 6 64 121 92 25 134 255 9 209 156 209 119 187 13 160 23 147 240 24 196 152 100 20 163 51 118 45 100 26 179 227 184 166 147 113 50 139]"
+	e1 = `ssW`
+	e3 = "sssss"
 
 	pubKey2 = pki.GetBLSPrivateKeyFromInt(999).GetPublicKey()
 
@@ -45,7 +44,6 @@ var (
 
 	buf1 []byte
 	buf2 []byte
-	e3=""
 )
 
 func TestString(test *testing.T) {
@@ -62,7 +60,7 @@ func TestString(test *testing.T) {
 	}
 
 	pong1 := NewPongMessage(p2, pubKeys, leaderPubKey, 0)
-	
+
 	if !strings.HasPrefix(pong1.String(), e2) {
 		test.Errorf("expect: %v, got: %v", e2, pong1.String())
 	}
